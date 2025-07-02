d23bc6568600785eb31d9f4f15e5083f3ef39b36
IgorYonezawa <igoryonezawa@gmail.com>
2025-07-02 23:27:12 +0000

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/index.css'
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './styles/styles.css'
-import './styles/index.css'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext';
 
 
